6c0c734c2545b67e5c4ff47d8d50f9bcfd1667fb
Muhammad Irfan Ali <me@codewithirfan.com>
2025-04-25 15:27:26 +0500

    TMDb Theme

--- app/src/main/java/com/irfangujjar/tmdb/MainActivity.kt
+++ app/src/main/java/com/irfangujjar/tmdb/MainActivity.kt
@@ -11,17 +11,17 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.irfangujjar.tmdb.ui.theme.TmdbandroidTheme
+import com.irfangujjar.tmdb.ui.theme.TMDbTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            TmdbandroidTheme {
+            TMDbTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "TMDb",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -41,7 +41,7 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    TmdbandroidTheme {
+    TMDbTheme {
         Greeting("Android")
     }
 }
