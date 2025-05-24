5a8365d081f1bda43067a8f48d0d4f26209bd444
Muhammad Irfan Ali <me@codewithirfan.com>
2025-05-25 01:42:01 +0500

    Removed unused dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-    alias(libs.plugins.google.services)
 }
 
 android {
@@ -52,8 +51,6 @@ dependencies {
     implementation(libs.androidx.material3)
 
     implementation(libs.androidx.core.splashscreen)
-    implementation(platform(libs.firebase.bom))
-    implementation(libs.firebase.analytics)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
